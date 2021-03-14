e267d838ce440475a5e2c28643176cec6da61d8e
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2021-03-14 13:13:59 -0400

    styling day 2

--- student-letters/src/components/Main.js
+++ student-letters/src/components/Main.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 function Main(props) {
     return (
-        <div className = "Main">
+        <div className = "Main-container">
+            <div className = "Main">
    <h3>Initials: TW </h3>
    <h3>Location: CA </h3>
    <h3>Letter:Don’t worry about how hard it gets.  You got this. Look to your classmates they are your backbone.  You will never find better people to go though this experience with then them.  Study the basics, go to office hours every night, even if you don’t have questions just go.  Trust that everyone near you has the same struggles and is looking for someone just like you. Most importantly, YOU. CAN. DO. THIS </h3>
-
+   </div>
    {/* <h3>Initials: {letters.intials}</h3>
    <h3>Location: {letters.location} </h3>
    <h3>Letter: {letters.letter} </h3> */}
