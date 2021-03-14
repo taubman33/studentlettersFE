4e7486b05446ae7c2c94b204f5e703c4b57ffa3e
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2021-03-13 20:51:39 -0500

    building up react front end

---
+++ student-letters/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <h2> 2021 JT ND MA KZ</h2>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ student-letters/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className = 'Header'>
+            <h1> Letters to the Next Cohort</h1>
+
+        </div>
+    );
+}
+
+export default Header;
