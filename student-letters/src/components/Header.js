8ac6c9fa8528000d785b39cbbccf6ee6eac67274
michaelannawesome <michaelannawesome@gmail.com>
2021-03-15 10:46:28 -0700

    css changes

--- student-letters/src/components/Header.js
+++ student-letters/src/components/Header.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Header(props) {
-    return (
-        <div className = 'Header'>
-            <div className="Header-title">
-            <h1> Letters to the Next Cohort</h1>
-            </div>
+  return (
+    <div className="App-header">
+      {/* <div className="Header-title"> */}
+      <h1 className="text"> Letters to the Next Cohort</h1>
 
-            <div className="Header-subtext">
-            <h1> From the 11/30-03/04 Cohort</h1>
-            </div>       
-            
-        </div>
-    );
+      <div className="Header-subtext">
+        <h6 className="text sub"> From the 11/30-03/04 Cohort</h6>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
