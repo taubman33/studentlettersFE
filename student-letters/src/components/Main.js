375509611d0ec9351838a2f29f452bc602d6dfc1
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2021-03-16 13:47:27 -0400

    trying to render

--- student-letters/src/components/Main.js
+++ student-letters/src/components/Main.js
@@ -7,37 +7,36 @@ export default function Main () {
    const [letters, setLetters] = useState([])
    const url = "https://sei1130-letters.herokuapp.com/students"
 
-
+   let letterArray = []
    //pulling data
    useEffect(() => {
+      
         fetch(url)
       .then(res => {
       return res.json();
        })
       .then(res => {
-     console.log("success!", res);
-     console.log("success!", res[0])
-     console.log("success!", res[0].initials)
+     console.log("success!", res)
+   //   setLetters(res)
+     letterArray.push(res)
+   //   setLetters(letterArray)
+     console.log(letterArray)
       })
       .catch(err => {
      console.log("something went wrong...", err);
       });
        }, )      
 
-
+       console.log(letterArray)
    //raising number to scroll through pulled array
     function raiseNum() {
         setNum(num+ 1 )
-         
          console.log(num)
-         // console.log(letters.length)
-         // if (setNum > letters.length) {
-         //  setNum(0)
-         //  console.log("max length reached")
        }
    
        console.log(num)
-    
+       console.log(letterArray)
+       console.log(letterArray[0])
    
    return (     
         
@@ -46,8 +45,9 @@ export default function Main () {
             <div className="Main-text">
 
             <h3>Initials: works </h3>
- 
-            {/* <h3>Initials:  { letters && letters[0].initials}</h3>
+        
+{/*    
+            <h3>Initials:  { letterArray && letterArray[0].initials}</h3>
              */}
              
             {/* <h3>Location: {letters && letters[num].location} </h3>
@@ -58,9 +58,6 @@ export default function Main () {
             </div>
 
             <button id="next-letter" onClick={raiseNum}>Next Letter</button>
-            
-
-          
             </div>
         </div>
        );
