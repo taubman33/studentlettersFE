69fb168d830720e8d62714129b4d33dc7f185da6
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2021-03-16 14:22:12 -0400

    rendering and button working

--- student-letters/src/components/Main.js
+++ student-letters/src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-
 export default function Main () {
 
    const [letter, setLetter] = useState({});
@@ -19,18 +18,24 @@ export default function Main () {
       function raiseNum() {
          setNum(num+ 1 )
           console.log(num)
+          if (num > letter.length){
+             setNum (0);
+          }
         }
-    
+       
+        
+        console.log(letter.length)
         console.log(num)
+        console.log(letter[num])
 
    
 
    return (
-       <div className="stateDetails">
-
-           <h2>{`Student: ${letter.initials}`}</h2>
-           <h2>{`Location: ${letter.location}`}</h2>
-           <h2>{`Location: ${letter.letter}`}</h2>
+       <div className="Main">
+           
+            <h2>{`Student: ${letter[num].initials}`}</h2>
+           <h2>{`Location: ${letter[num].location}`}</h2>
+           <h2>{`Location: ${letter[num].letter}`}</h2> 
            <button id="next-letter" onClick={raiseNum}>Next Letter</button>
 
        </div>
