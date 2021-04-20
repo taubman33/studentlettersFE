bed8493bb90326d159c52a031a776d25115e98e9
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2021-04-20 10:23:23 -0400

    more cleaning

--- student-letters/src/components/Nav.js
+++ student-letters/src/components/Nav.js
@@ -5,18 +5,25 @@ function Nav(props) {
   return (
     <div className="Navbar-container">
       <div className="Navbar">
+
+        
         <Link to="/">
           <button id="home-button" className="button">
             {" "}
             <h3>Home</h3>
           </button>
         </Link>
+
+
+
         <Link to="/letters">
           <button id="home-button" className="button">
             {" "}
             <h3>Letters</h3>
           </button>
         </Link>
+
+
       </div>
     </div>
   );
